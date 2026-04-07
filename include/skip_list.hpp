5fe0d3a49b52aed8669c217a926d205b3d4ee7e1
Nathan Jacobson <nathan@nathanjacobson.ca>
2026-04-07 14:35:04 -0700

    added Node and SkipList class to skip_list.hpp

--- include/skip_list.hpp
+++ include/skip_list.hpp
@@ -1,1 +1,25 @@
 #pragma once
+#include <vector>
+
+const int maxSkipListLevell = 5;
+
+class Node {
+public:
+    int data;
+    std::vector<Node*> next; 
+    Node(int data, int level) : data(data), next(level + 1, nullptr) {}
+};
+
+template <typename T>
+class SkipList {
+public:
+    SkipList();
+
+    void Insert(int data);
+    void Remove(int data);
+    bool Search(int data) const;
+    void Print() const;
+private:
+    Node* head;
+    int level;
+};
