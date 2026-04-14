cd28a8a346cf27039ee32b8fcefd8b67786c8f44
“Milad244” <145618774+Milad244@users.noreply.github.com>
2026-04-14 16:30:15 -0700

    Refactored AVL
    Our comparisons will need to be independent of hashing algorithms, so I am removed the data part.

--- include/balanced_tree.hpp
+++ include/balanced_tree.hpp
@@ -1,191 +1,56 @@
 #pragma once
-#include <cmath>
-#include <iostream>
-#include <queue>
 
-using std::cout;
-using std::queue;
-
-template <typename T>
-struct AVLNode {
-    AVLNode<T>* left;
-    AVLNode<T>* right;
+class AVLNode {
+public:
+    AVLNode* left;
+    AVLNode* right;
     int key;
-    T data;
     int height;
 
-    AVLNode(int key, const T& data)
-        : key(key),
-          data(data),
-          left(nullptr),
-          right(nullptr),
-          height(0)
-    {}
+    AVLNode(int key) : key(key), left(nullptr), right(nullptr), height(0) {}
 };
 
-template <typename T>
 class AVLTree {
 public:
-    AVLTree();
-    ~AVLTree();
+    AVLTree() = default;
+    ~AVLTree() = default;
 
     // Inserts a node to the tree (does not insert if the key is a duplicate)
-    void insert(int key, const T& data) {
-        this->root = insertHelper(this->root, key, data);
-        updateHeight(this->root);
-        return;
-    }
+    void insert(int key);
 
     // Returns the height of the node (0 if node is null)
-    int getHeight(AVLNode<T>* node) const {
-        if (!node) return 0;
-        return node->height;
-    }
+    int getHeight(AVLNode* node) const;
 
     // Prints the level order traversal of the tree
-    void levelOrder() {
-        if (!this->root) return;
-
-        queue<AVLNode<T>*> q;
-        q.push(this->root);
-        
-        while (!q.empty()) {
-            AVLNode<T>* node = q.front();
-            q.pop();
-
-            cout << "Key: " << node->key << "Data: " << node->data << " ";
-
-            if (q.empty()) cout << '\n';
-
-            if (node->left) q.push(node->left);
-            if (node->right) q.push(node->right);
-        }
-    }
+    void levelOrder() const;
 
     // Testing function to ensure the tree has the BST sorted property
-    bool isSorted() {
-        isSortedHelper(this->root);
-    }
+    bool isSorted() const;
 
     // Testing function to ensure the tree has the AVL balanced property
-    bool isBalanced() {
-        isBalancedHelper(this->root);
-    }
+    bool isBalanced() const;
 
 private:
-    AVLNode<T>* root = nullptr;
+    AVLNode* root = nullptr;
 
     // Updates height of the given node
-    void updateHeight(AVLNode<T>* updateNode) {
-        if (!updateNode) return;
-        updateNode->height = 1 + std::max(getHeight(updateNode->left), getHeight(updateNode->right));
-    }
+    void updateHeight(AVLNode* updateNode);
 
-    // Rotates a rightward chain, to the left and returns the new root (null if node or node->right are null)
-    AVLNode<T>* rotateLeft(AVLNode<T>* node) {
-        if (!node || !node->right) return nullptr;
-        AVLNode<T>* z = node;
-        AVLNode<T>* y = node->right;
-        z->right = y->left; // z takes over y's left child
-        y->left = z; // y becomes the new root
-        updateHeight(z);
-        updateHeight(y);
-        return y;
-    }
+    // Rotates a rightward chain to the left and returns the new root (null if node or node->right are null)
+    AVLNode* rotateLeft(AVLNode* node);
 
-    // Rotates a leftward chain, to the right and returns the new root (null if node or node->left are null)
-    AVLNode<T>* rotateRight(AVLNode<T>* node) {
-        if (!node || !node->left) return nullptr;
-        AVLNode<T>* z = node;
-        AVLNode<T>* y = node->left;
-        z->left = y->right; // z takes over y's right child
-        y->right = z; // y becomes the new root
-        updateHeight(z);
-        updateHeight(y);
-        return y;
-    }
+    // Rotates a leftward chain to the right and returns the new root (null if node or node->left are null)
+    AVLNode* rotateRight(AVLNode* node);
 
     // Returns true if the node satisfies the balance factor: |h(left) - h(right)| <= 1
-    bool isBalancedFactored(AVLNode<T>* node) {
-        return std::abs(getHeight(node->left) - getHeight(node->right)) <= 1;
-    }
+    bool isBalancedFactored(AVLNode* node) const;
 
-    AVLNode<T>* insertHelper(AVLNode<T>* insertAtNode, int key, const T& data) {
-        // Base case
-        if (!insertAtNode) {
-            return new AVLNode<T>(key, data);
-        }
-
-        // Recursively inserting based on the BST's sorted property
-        if (insertAtNode->key > key) insertAtNode->left = insertHelper(insertAtNode->left, key, data);
-        else if (insertAtNode->key < key) insertAtNode->right = insertHelper(insertAtNode->right, key, data);
-        else return insertAtNode; // Duplicate key case
-
-        updateHeight(insertAtNode);
-
-        // Ensuring balance factor is kept
-        if (!isBalancedFactored(insertAtNode)) {
-            insertAtNode = restructure(insertAtNode);
-        }
-
-        return insertAtNode;
-    }
+    AVLNode* insertHelper(AVLNode* insertAtNode, int key);
 
     // Rotates the tree where given a node with an unblanced factor
-    AVLNode<T>* restructure(AVLNode<T>* restructureAtNode) {
-        // An unbalanced node must have at least a child of relative height 2
-        
-        static_assert(restructureAtNode, "Unbalanced node must have at least a child of relative height 2");
+    AVLNode* restructure(AVLNode* restructureAtNode);
+    
+    bool isSortedHelper(AVLNode* node) const;
 
-        AVLNode<T>* z = restructureAtNode; // First unbalanced ancestor
-        AVLNode<T>* y; // Heightest child of z
-        AVLNode<T>* x; // Heightest child of y
-
-        // Getting y and z
-        if (getHeight(z->left) > getHeight(z->right)) y = z->left;
-        else y = z->right;
-
-        static_assert(y, "Unbalanced node must have at least a child of relative height 2");
-
-        if (getHeight(y->left) > getHeight(y->right)) x = y->left;
-        else x = y->right;
-
-        static_assert(x, "Unbalanced node must have at least a child of relative height 2");
-
-        if (z->right == y && y->right == x) return rotateLeft(z); // RR
-        else if (z->left == y && y->left == x) return rotateRight(z); // LL
-        else if (z->right == y && y->left == x) { // RL
-            z->right = rotateRight(y); // first make it into a rightward chain
-            return rotateLeft(z);
-        }
-        else if (z->left == y && y->right == x) { // LR
-            z->left = rotateLeft(y); // first make it into a leftward chain
-            return rotateRight(z);
-        }
-
-        return z;
-    }
-
-    bool isSortedHelper(AVLNode<T>* node) {
-        if (!node) return true;
-
-        if (node->left && node->left->key >= node->key) return false;
-        if (node->right && node->right->key <= node->key) return false;
-
-        bool leftCheck = isSortedHelper(node->left);
-        bool rightCheck = isSortedHelper(node->right);
-
-        return leftCheck && rightCheck;
-    }
-
-    bool isBalancedHelper(AVLNode<T>* node) {
-        if (!node) return true;
-
-        if (!isBalancedFactored(node)) return false;
-
-        bool leftCheck = isBalancedHelper(node->left);
-        bool rightCheck = isBalancedHelper(node->right);
-
-        return leftCheck && rightCheck;
-    }
+    bool isBalancedHelper(AVLNode* node) const;
 };
---
+++ src/balanced_tree.cpp
@@ -0,0 +1,158 @@
+#include "balanced_tree.hpp"
+#include <queue>
+#include <iostream>
+#include <cmath>
+#include <cassert>
+using std::queue;
+using std::cout;
+using std::abs;
+
+// Public Methods
+
+void AVLTree::insert(int key) {
+    this->root = insertHelper(this->root, key);
+    updateHeight(this->root);
+}
+
+int AVLTree::getHeight(AVLNode* node) const {
+    if (!node) return 0;
+    return node->height;
+}
+
+void AVLTree::levelOrder() const {
+    if (!this->root) return;
+
+    queue<AVLNode*> q;
+    q.push(this->root);
+    
+    while (!q.empty()) {
+        AVLNode* node = q.front();
+        q.pop();
+
+        cout << "Key: " << node->key;
+
+        if (q.empty()) cout << '\n';
+
+        if (node->left) q.push(node->left);
+        if (node->right) q.push(node->right);
+    }
+}
+
+bool AVLTree::isSorted() const {
+    return isSortedHelper(this->root);
+}
+
+bool AVLTree::isBalanced() const {
+    return isBalancedHelper(this->root);
+}
+
+// Private Methods
+
+void AVLTree::updateHeight(AVLNode* updateNode) {
+    if (!updateNode) return;
+    updateNode->height = 1 + std::max(getHeight(updateNode->left), getHeight(updateNode->right));
+}
+
+AVLNode* AVLTree::rotateLeft(AVLNode* node) {
+    if (!node || !node->right) return nullptr;
+    AVLNode* z = node;
+    AVLNode* y = node->right;
+    z->right = y->left; // z takes over y's left child
+    y->left = z; // y becomes the new root
+    updateHeight(z);
+    updateHeight(y);
+    return y;
+}
+
+AVLNode* AVLTree::rotateRight(AVLNode* node) {
+    if (!node || !node->left) return nullptr;
+    AVLNode* z = node;
+    AVLNode* y = node->left;
+    z->left = y->right; // z takes over y's right child
+    y->right = z; // y becomes the new root
+    updateHeight(z);
+    updateHeight(y);
+    return y;
+}
+
+bool AVLTree::isBalancedFactored(AVLNode* node) const {
+    return std::abs(getHeight(node->left) - getHeight(node->right)) <= 1;
+}
+
+AVLNode* AVLTree::insertHelper(AVLNode* insertAtNode, int key) {
+    // Base case
+    if (!insertAtNode) {
+        return new AVLNode(key);
+    }
+
+    // Recursively inserting based on the BST's sorted property
+    if (insertAtNode->key > key) insertAtNode->left = insertHelper(insertAtNode->left, key);
+    else if (insertAtNode->key < key) insertAtNode->right = insertHelper(insertAtNode->right, key);
+    else return insertAtNode; // Duplicate key case
+
+    updateHeight(insertAtNode);
+
+    // Ensuring balance factor is kept
+    if (!isBalancedFactored(insertAtNode)) {
+        insertAtNode = restructure(insertAtNode);
+    }
+
+    return insertAtNode;
+}
+
+AVLNode* AVLTree::restructure(AVLNode* restructureAtNode) {
+    // An unbalanced node must have at least a child of relative height 2
+    
+    assert(restructureAtNode && "Unbalanced node must have at least a child of relative height 2");
+
+    AVLNode* z = restructureAtNode; // First unbalanced ancestor
+    AVLNode* y; // Heightest child of z
+    AVLNode* x; // Heightest child of y
+
+    // Getting y and z
+    if (getHeight(z->left) > getHeight(z->right)) y = z->left;
+    else y = z->right;
+
+    assert(y && "Unbalanced node must have at least a child of relative height 2");
+
+    if (getHeight(y->left) > getHeight(y->right)) x = y->left;
+    else x = y->right;
+
+    assert(x && "Unbalanced node must have at least a child of relative height 2");
+
+    if (z->right == y && y->right == x) return rotateLeft(z); // RR, so rotate left
+    else if (z->left == y && y->left == x) return rotateRight(z); // LL, so rotate right
+    else if (z->right == y && y->left == x) { // RL
+        z->right = rotateRight(y); // first make it into a rightward chain
+        return rotateLeft(z); // now it's RR, so rotate left
+    }
+    else if (z->left == y && y->right == x) { // LR
+        z->left = rotateLeft(y); // first make it into a leftward chain
+        return rotateRight(z); // then it's LL, so rotate right
+    }
+
+    return z;
+}
+
+bool AVLTree::isSortedHelper(AVLNode* node) const {
+    if (!node) return true;
+
+    if (node->left && node->left->key >= node->key) return false;
+    if (node->right && node->right->key <= node->key) return false;
+
+    bool leftCheck = isSortedHelper(node->left);
+    bool rightCheck = isSortedHelper(node->right);
+
+    return leftCheck && rightCheck;
+}
+
+bool AVLTree::isBalancedHelper(AVLNode* node) const {
+    if (!node) return true;
+
+    if (!isBalancedFactored(node)) return false;
+
+    bool leftCheck = isBalancedHelper(node->left);
+    bool rightCheck = isBalancedHelper(node->right);
+
+    return leftCheck && rightCheck;
+}
