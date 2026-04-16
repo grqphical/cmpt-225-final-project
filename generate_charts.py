0790faeb6a0d0b404bfe8538a457682ad8295187
Nathan Jacobson <nathan@nathanjacobson.ca>
2026-04-15 17:10:32 -0700

    added CLI args for chart generation script

--- generate_charts.py
+++ generate_charts.py
@@ -1,5 +1,6 @@
 """
 Expects data in the form:
+
 Name,Skip List,AVL Tree
 Random Insertion,1000,1500
 Ordered Insertion,1200,800
@@ -9,18 +10,27 @@ Removal,380,240
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
 
-df = pd.read_csv("data.csv")
+parser = argparse.ArgumentParser(usage="Generates charts from CSV files")
+parser.add_argument("--title", "-t", required=True)
+parser.add_argument("--output", "-o", default="output.png")
+parser.add_argument("file")
+
+args = parser.parse_args()
+
+df = pd.read_csv(args.file)
 
 df.set_index("Name", inplace=True)
 
 ax = df.plot(kind="bar", figsize=(10, 6))
 
-plt.title("Skip Lists vs AVL Trees")
+plt.title(args.title)
 plt.xlabel("Test Cases")
 plt.ylabel("Time (ms)")
 plt.xticks(rotation=0)
-plt.legend(title="Category")
+plt.legend(title="Data Structure")
 
 plt.tight_layout()
-plt.show()
+plt.savefig(args.output)
+print(f"Saved chart to {args.output}")
